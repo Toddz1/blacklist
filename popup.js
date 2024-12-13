839c26100eac015d27dec8e151b70ee6654a98b3
toddz <toddhorseman@qq.com>
2024-12-14 01:20:36 +0800

    init

---
+++ popup.js
@@ -0,0 +1,6 @@
+// popup.js
+
+document.getElementById('blockButton').addEventListener('click', function() {
+    // Logic to add the current site to the blacklist
+    alert('Site blocked!');
+  });
